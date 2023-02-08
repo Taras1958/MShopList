ddb46cd463840aecb8cf80e6f7d3a0b10214c0b6
Taras1958 <118650875+Taras1958@users.noreply.github.com>
2023-02-08 09:04:38 +0300

    Presentation слой часть I

--- app/src/main/java/ru/stambul4you/mshoplist/presentation/MainActivity.kt
+++ app/src/main/java/ru/stambul4you/mshoplist/presentation/MainActivity.kt
@@ -2,11 +2,20 @@ package ru.stambul4you.mshoplist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import ru.stambul4you.mshoplist.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.shopList.observe(this) {
+            Log.d("MyLog", it.toString())
+        }
+        viewModel.getShopList()
     }
 }
