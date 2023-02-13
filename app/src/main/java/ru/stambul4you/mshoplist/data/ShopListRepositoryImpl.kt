d77d93ec3ef26c986f9d82f3a0e59481da5be155
Taras1958 <118650875+Taras1958@users.noreply.github.com>
2023-02-13 10:05:35 +0300

    Bugs in RecyclerView

--- app/src/main/java/ru/stambul4you/mshoplist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/ru/stambul4you/mshoplist/data/ShopListRepositoryImpl.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import ru.stambul4you.mshoplist.domain.ShopItem
 import ru.stambul4you.mshoplist.domain.ShopListRepository
+import kotlin.random.Random
 
 object ShopListRepositoryImpl: ShopListRepository {
 
@@ -14,8 +15,8 @@ object ShopListRepositoryImpl: ShopListRepository {
     private var autoIncrementId = 0
 
     init {
-        for(i in 0 until 10) {
-            val item = ShopItem("Name $i", i, true)
+        for(i in 0 until 1000) {
+            val item = ShopItem("Name $i", i, Random.nextBoolean())
             addShopItem(item)
         }
     }
