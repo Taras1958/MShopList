b6f40cc8a4495787b9f4895695f53875ac93d710
Taras1958 <118650875+Taras1958@users.noreply.github.com>
2023-02-15 10:38:38 +0300

    Add Listeners

--- app/src/main/java/ru/stambul4you/mshoplist/presentation/MainActivity.kt
+++ app/src/main/java/ru/stambul4you/mshoplist/presentation/MainActivity.kt
@@ -2,9 +2,12 @@ package ru.stambul4you.mshoplist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import ru.stambul4you.mshoplist.R
+import ru.stambul4you.mshoplist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,27 +15,71 @@ class MainActivity : AppCompatActivity() {
     private lateinit var shopListadapter: ShopListAdapter
 
 
-        override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupRecyclerView()
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
-                shopListadapter.shopList = it
+            shopListadapter.shopList = it
         }
 
     }
-            private fun setupRecyclerView(){
-                val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
-                with(rvShopList) {
-                shopListadapter = ShopListAdapter()
-                adapter = shopListadapter
-                recycledViewPool.setMaxRecycledViews(
-                    ShopListAdapter.VIEW_TYPE_ENABLED,
-                    ShopListAdapter.MAX_POOL_SIZE)
-                recycledViewPool.setMaxRecycledViews(
-                    ShopListAdapter.VIEW_TYPE_DISABLED,
-                    ShopListAdapter.MAX_POOL_SIZE)
-                }
+
+    private fun setupRecyclerView() {
+        val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
+        with(rvShopList) {
+            shopListadapter = ShopListAdapter()
+            adapter = shopListadapter
+            recycledViewPool.setMaxRecycledViews(
+                ShopListAdapter.VIEW_TYPE_ENABLED,
+                ShopListAdapter.MAX_POOL_SIZE
+            )
+            recycledViewPool.setMaxRecycledViews(
+                ShopListAdapter.VIEW_TYPE_DISABLED,
+                ShopListAdapter.MAX_POOL_SIZE
+            )
+        }
+
+        setupLongClickListener()
+        setupClickListener()
+        setupSwipeListener(rvShopList)
+    }
+
+    private fun setupSwipeListener(rvShopList: RecyclerView) {
+        val callback = object : ItemTouchHelper.SimpleCallback(
+            0,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
             }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val item = shopListadapter.shopList[viewHolder.adapterPosition]
+                viewModel.deleteShopItem(item)
+            }
+
+        }
+
+        val itemTouchHelper = ItemTouchHelper(callback)
+        itemTouchHelper.attachToRecyclerView(rvShopList)
+    }
+
+    private fun setupClickListener() {
+        shopListadapter.onShopItemClickListener = {
+            Log.d("MyActivity", it.toString())
+        }
+    }
+
+    private fun setupLongClickListener() {
+        shopListadapter.onShopItemLongClickListener = {
+            viewModel.changeEnableState(it)
+        }
+    }
+
 }
