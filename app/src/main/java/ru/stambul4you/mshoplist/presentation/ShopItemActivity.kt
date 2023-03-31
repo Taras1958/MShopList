11b2c855ee0131ecc9d50511246ea559d1fc3423
Taras1958 <118650875+Taras1958@users.noreply.github.com>
2023-03-31 09:21:45 +0300

    Create Second Screen Layouts

---
+++ app/src/main/java/ru/stambul4you/mshoplist/presentation/ShopItemActivity.kt
@@ -0,0 +1,13 @@
+package ru.stambul4you.mshoplist.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.stambul4you.mshoplist.R
+
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
