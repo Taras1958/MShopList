40e150925f47015eb275671efea22ad0d34269c8
Taras1958 <118650875+Taras1958@users.noreply.github.com>
2023-04-03 09:57:20 +0300

    Create ShopItemViewModel finish

--- app/src/main/java/ru/stambul4you/mshoplist/presentation/ShopItemActivity.kt
+++ app/src/main/java/ru/stambul4you/mshoplist/presentation/ShopItemActivity.kt
@@ -1,13 +1,14 @@
 package ru.stambul4you.mshoplist.presentation
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import ru.stambul4you.mshoplist.R
 
-
 class ShopItemActivity : AppCompatActivity() {
+    private lateinit var viewModel: ShopItemViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
+//        viewModel.errorInputName.value = false
     }
 }
